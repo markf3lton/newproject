afb00f187b284bd2a2adbad3d03a43df867c3d72
Mark Felton <mark.felton@acquia.com>
2022-01-15 22:22:28 +0000

    Added BLT.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -784,3 +784,12 @@ if (file_exists('/var/www/site-php')) {
 
 
 
+require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
+/**
+ * IMPORTANT.
+ *
+ * Do not include additional settings here. Instead, add them to settings
+ * included by `blt.settings.php`. See BLT's documentation for more detail.
+ *
+ * @link https://docs.acquia.com/blt/
+ */
